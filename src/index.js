07f114226d7e232dcee5a0819f3f019162505007
Rohan Gupta <76099198+Rohan1572@users.noreply.github.com>
2021-10-20 18:59:14 +0530

    1900

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph</p>
+    <h1 className="heading">My Favourite Cuisines</h1>
+    {/* <ul>
+      <li>Pasta</li>
+      <li>Noodles</li>
+      <li>Rajma Chawal</li>
+    </ul> */}
+    <div>
+      <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2021/05/arrabiata-pasta-1.jpg"></img>
+      <img src="https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodlesH2.jpg"></img>
+      <img src="https://s9c2c8i7.rocketcdn.me/wp-content/uploads/2017/08/rajma-chawal-1.jpg"></img>
+    </div>
   </div>,
   document.getElementById("root")
 );
